d5760563d8901c3f6d0178de94c6b5fd88526ab4
sghawana <59884048+sghawana@users.noreply.github.com>
2024-09-24 23:18:11 +0530

    finishung

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 
 from main_functions import  *
 
-#### ----> Instead of edge list all functions use graph adjacency lists as input<---- ###
+#### ----> All functions use graph adjacency lists as input<---- ###
 if __name__ == "__main__":
 
-    ############ Answer qn 1-4 for wiki-vote data #################################################
+    ############ For wiki-vote data #################################################
     # Import wiki-vote.txt
     # nodes_connectivity_list is a nx2 numpy array, where every row 
     # is an edge connecting i->j (entry in the first column is node i, 
@@ -23,7 +23,6 @@ if __name__ == "__main__":
     graph_wiki = revert_node_ids(new_graph_wiki, node_id_map_wiki)
     '''
     
-    # This is for question no. 1
     # graph_partition: graph_partitition is a nx1 numpy array where the rows corresponds to nodes in the network (0 to n-1) and
     #                  the elements of the array are the community ids of the corressponding nodes.
     #                  Follow the convention that the community id is equal to the lowest nodeID in that community.
@@ -32,24 +31,19 @@ if __name__ == "__main__":
     graph_partition_wiki = dict_to_np(graph_partition_wiki)
     # First parameter is new graph with removed edges(if required)
 
-    # This is for question no. 2. Use the function 
-    # written for question no.1 iteratetively within this function.
     # community_mat is a n x m matrix, where m is the number of levels of Girvan-Newmann algorithm and n is the number of nodes in the network.
     # Columns of the matrix corresponds to the graph_partition which is a nx1 numpy array, as before, corresponding to each level of the algorithm. 
     community_mat_wiki = girvan_newman(new_graph_wiki)
 
-    # This is for question no. 3
-    # Visualise dendogram for the communities obtained in question no. 2.
     # Save the dendogram as a .png file in the current directory.
-    visualise_dendogram(community_mat_wiki, 'wiki')
+    #visualise_dendogram(community_mat_wiki, 'wiki')
 
-    # This is for question no. 4
     # run one iteration of louvain algorithm and return the resulting graph_partition. The description of
     # graph_partition vector is as before. Show the resulting communities after one iteration of the algorithm.
-    #graph_partition_louvain_wiki = louvain_one_iter(new_graph_wiki)
+    graph_partition_louvain_wiki = louvain_one_iter(new_graph_wiki)
 
 
-    ############ Answer qn 1-4 for bitcoin data #################################################
+    ############ For bitcoin data #################################################
     # Import lastfm_asia_edges.csv
     # nodes_connectivity_list is a nx2 numpy array, where every row 
     # is an edge connecting i<->j (entry in the first column is node i, 
@@ -65,18 +59,13 @@ if __name__ == "__main__":
     graph_wiki = revert_node_ids(new_graph_wiki, node_id_map_wiki)
     '''
 
-    # Question 1
     _, graph_partition_lastfm = girvan_newman_one_level(new_graph_lastfm, True)
     graph_partition_lastfm = dict_to_np(graph_partition_lastfm)
     # First parameter is new graph with removed edges(if required)
 
-    # Question 2
     community_mat_lastfm = girvan_newman(new_graph_lastfm)
     
+    #visualise_dendogram(community_mat_lastfm, 'lastfm')
 
-    # Question 3
-    visualise_dendogram(community_mat_lastfm, 'lastfm')
-
-    # Question 4
-    #graph_partition_louvain_lastfm = louvain_one_iter(new_graph_lastfm)
+    graph_partition_louvain_lastfm = louvain_one_iter(new_graph_lastfm)
 
